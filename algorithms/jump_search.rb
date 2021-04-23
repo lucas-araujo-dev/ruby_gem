cfbf21a4cf4f3618d72056b6625acae15751b481
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-04-23 20:36:04 -0300

    chore(algorithms): added jump search

---
+++ algorithms/jump_search.rb
@@ -0,0 +1,41 @@
+def jump_search (array, num)
+  size = array.length
+  step = Math.sqrt(size)
+
+  prev = 0
+
+  while array[[step,size].min] < num do
+    prev = step
+
+    step += Math.sqrt(size)
+
+    return -1 if prev >= size
+  end
+
+  while array[prev] < num do
+    prev += 1
+
+    return -1 if prev == [step, size].min
+  end
+
+  if array[prev] == num
+    return prev.to_i
+  end
+
+  return -1
+end
+
+array = Array.new(10) { rand(1..20) }.sort
+num = rand(1..20)
+
+
+puts "Array => #{array}"
+puts "Numero: #{num}"
+
+result = jump_search(array, num)
+
+unless result == -1
+  puts "O index do numero escolhido eh: #{result}"
+else
+  puts "O numero nao esta presente no array"
+end
