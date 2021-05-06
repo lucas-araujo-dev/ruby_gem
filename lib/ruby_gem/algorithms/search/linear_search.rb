6ceba14d483e8b7cb9d3e91792f75c7ce79c7edc
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-05-06 05:50:49 -0300

    refactor(lib/ruby_gem/algorithms/search): transform linear search into class

--- lib/ruby_gem/algorithms/search/linear_search.rb
+++ lib/ruby_gem/algorithms/search/linear_search.rb
@@ -1,25 +1,17 @@
-#Inputs
+module RubyGem
+  module Algorithms
+    module Search
+      class LinearSearch
+        def self.search(array, num)
+          puts "Array => #{array}"
+          puts "Numero: #{num}"
 
-def linear_search(array, num)
-  puts "Array => #{array}"
-  puts "Numero: #{num}"
-
-  array.each_with_index do |item, index|
-    return index if item == num
+          array.each_with_index do |item, index|
+            return index if item == num
+          end
+          return -1
+        end
+      end
+    end
   end
-  return -1
-end
-
-puts `clear`
-
-#Inputs
-array = Array.new(10) { rand(1..20) }
-num = rand(1..20)
-
-result = linear_search(array, num)
-
-unless result == -1
-  puts "O index do numero escolhido eh: #{result}"
-else
-  puts "O numero nao esta presente no array"
 end
