46d5dd903b718fe4e9e39a56c26116c110e8b33b
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2021-05-06 05:40:44 -0300

    refactor(lib/ruby_gem/algorithms/search): transform jump search into class

--- lib/ruby_gem/algorithms/search/jump_search.rb
+++ lib/ruby_gem/algorithms/search/jump_search.rb
@@ -1,41 +1,35 @@
-def jump_search (array, num)
-  size = array.length
-  step = Math.sqrt(size).floor
+module RubyGem
+  module Algorithms
+    module Search
+      class JumpSearch
 
-  prev = 0
+        def self.search (array, num)
+          size = array.length
+          step = Math.sqrt(size).floor
 
-  while array[([step,size].min)-1] < num do
-    prev = step
+          prev = 0
 
-    step += Math.sqrt(size).floor
+          while array[([step, size].min) - 1] < num do
+            prev = step
 
-    return -1 if prev >= size
+            step += Math.sqrt(size).floor
+
+            return -1 if prev >= size
+          end
+
+          while array[prev] < num do
+            prev += 1
+
+            return -1 if prev == [step, size].min
+          end
+
+          if array[prev] == num
+            return prev.to_i
+          end
+
+          return -1
+        end
+      end
+    end
   end
-
-  while array[prev] < num do
-    prev += 1
-
-    return -1 if prev == [step, size].min
-  end
-
-  if array[prev] == num
-    return prev.to_i
-  end
-
-  return -1
-end
-
-array = Array.new(10) { rand(1..20) }.sort
-num = rand(1..20)
-
-
-puts "Array => #{array}"
-puts "Numero: #{num}"
-
-result = jump_search(array, num)
-
-unless result == -1
-  puts "O index do numero escolhido eh: #{result}"
-else
-  puts "O numero nao esta presente no array"
 end
